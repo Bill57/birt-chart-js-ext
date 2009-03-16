077adae4d2d70972e4991ced4db07f0a7c11de73
lionel.wyl <lionel.wyl@c5db784a-11d7-11de-8090-533a57e34bb9>
2009-03-16 04:27:27 +0000

    Initial draft

---
+++ trunk/BirtChartJsExt/WebContent/js/chartAjaxAPI.js
@@ -0,0 +1,183 @@
+BirtChart = function() {
+
+}
+
+BirtChart.prototype = {
+	setDataXML : function(xml) {
+		if (typeof xml == 'string') {
+			this.dataXML = xml;
+		} else if (xml instanceof ChartModel) {
+			this.dataXML = xml.getXML();
+		}
+	},
+	setDataURL : function(url) {
+		this.dataURL = url;
+	},
+	render : function(div) {
+		var url;
+		if (this.dataURL) {
+			url = "chart?dataURL=" + this.dataURL;
+		} else {
+			url = "chart?dataXML=" + this.dataXML;
+		}
+		var req;
+		if (typeof XMLHttpRequest != "undefined") {
+			req = new XMLHttpRequest();
+		} else if (window.ActiveXObject) {
+			req = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		req.open("GET", url, true);
+		req.onreadystatechange = function() {
+			return function() {
+				if (req.readyState == 4 && req.status == 200) {
+					var xmlNode = req.responseXML.getElementsByTagName("chart")[0];
+					var html = xmlNode.childNodes[0].nodeValue;
+					var chartDiv = (typeof div == 'string') ? document
+							.getElementById(div) : div;
+					chartDiv.innerHTML = html;
+				}
+			}(div);
+		};
+		req.send(null);
+	}
+
+}
+
+StringBuffer = function(str) {
+	this.buffer = [];
+	if (str) {
+		this.buffer.push(str);
+	}
+}
+
+StringBuffer.prototype = {
+	append : function(string) {
+		this.buffer.push(string);
+		return this;
+	},
+
+	toString : function() {
+		return this.buffer.join("");
+	}
+}
+
+XMLWriter = function() {
+	this.buf = new StringBuffer();
+	this.bPairedFlag = true;
+}
+
+XMLWriter.prototype = {
+	attribute : function(attName, attValue) {
+		this.buf.append(' ' + attName + "=\"" + attValue + '\"');
+	},
+	openTag : function(tagName) {
+		if (!this.bPairedFlag) {
+			this.buf.append('>');
+		}
+		this.bPairedFlag = false;
+		this.buf.append('<').append(tagName);
+	},
+	closeTag : function(tagName) {
+		if (!this.bPairedFlag) {
+			this.buf.append("/>");
+		} else {
+			this.buf.append("</").append(tagName).append('>');
+		}
+		this.bPairedFlag = true;
+	},
+	text : function(value) {
+		if (!this.bPairedFlag) {
+			this.buf.append('>');
+			this.bPairedFlag = true;
+		}
+
+		this.buf.append(value);
+	},
+	getXML : function() {
+		return this.buf.toString();
+	}
+}
+
+ChartModel = function(width, height) {
+	this.type = "Bar";
+	this.format = "png";
+	this.dimension = "2d";
+	this.width = width;
+	this.height = height;
+}
+
+ChartModel.prototype = {
+	setFormat : function(format) {
+		this.format = format;
+	},
+	setType : function(type) {
+		this.type = type;
+	},
+	setDimension : function(dimension) {
+		this.dimension = dimension;
+	},
+	setTitle : function(title) {
+		this.title = title;
+	},
+	setScript : function(script) {
+		this.script = script;
+	},
+	setCategories : function(array) {
+		if (isArray(array)) {
+			this.categories = array;
+		}
+	},
+	setValues : function(array) {
+		if (isArray(array)) {
+			this.values = array;
+		}
+	},
+
+	getXML : function() {
+		var writer = new XMLWriter();
+		writer.openTag("chart");
+		writer.attribute("type", this.type);
+		writer.attribute("format", this.format);
+		writer.attribute("width", this.width);
+		writer.attribute("height", this.height);
+		writer.attribute("dimension", this.dimension);
+		writer.attribute("title", this.title);
+
+		if (this.categories) {
+			writer.openTag("categories");
+			for ( var i = 0; i < this.categories.length; i++) {
+				writer.openTag("category");
+				writer.attribute("label", this.categories[i]);
+				writer.closeTag("category");
+			}
+			writer.closeTag("categories");
+		}
+
+		if (this.values) {
+			writer.openTag("dataset");
+			for (i = 0; i < this.values.length; i++) {
+				writer.openTag("set");
+				writer.attribute("value", this.values[i]);
+				writer.closeTag("set");
+			}
+			writer.closeTag("dataset");
+		}
+
+		if (this.script) {
+			writer.openTag("script");
+			writer.text(this.script);
+			writer.closeTag("script");
+		}
+
+		writer.closeTag("chart");
+		return writer.getXML();
+	}
+}
+
+function isArray() {
+	if (typeof arguments[0] == 'object') {
+		var criterion = arguments[0].constructor.toString().match(/array/i);
+		return (criterion != null);
+	}
+	return false;
+}
---
+++ trunk/BirtChartJsExt/src/org/bcje/servlets/ChartServlet.java
@@ -0,0 +1,159 @@
+
+package org.bcje.servlets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bcje.model.ChartModel;
+import org.bcje.utils.ChartConvertor;
+import org.bcje.utils.ChartXMLParser;
+import org.eclipse.birt.chart.model.Chart;
+import org.eclipse.birt.chart.model.attribute.Bounds;
+import org.eclipse.birt.chart.viewer.internal.util.ChartImageManager;
+import org.eclipse.birt.chart.viewer.internal.util.ImageHTMLEmitter;
+
+/**
+ * 
+ */
+
+public class ChartServlet extends HttpServlet
+{
+
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = -970069279731222139L;
+
+	private ServletContext context;
+
+	public void init( ServletConfig config ) throws ServletException
+	{
+		super.init( config );
+		this.context = config.getServletContext( );
+
+	}
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response )
+			throws IOException, ServletException
+	{
+		String html = null;
+		String xml = request.getParameter( "dataXML" ); //$NON-NLS-1$
+		String url = request.getParameter( "dataURL" ); //$NON-NLS-1$
+		if ( url != null )
+		{
+			xml = loadXML( url );
+		}
+		ChartModel chart = null;
+
+		try
+		{
+			Chart cm = null;
+			if ( xml != null )
+			{
+				chart = new ChartXMLParser( xml ).createChart( );
+				cm = ChartConvertor.convertToBIRTChart( chart );
+			}
+			if ( cm != null )
+			{
+				// Set size in chart model
+				Bounds bounds = cm.getBlock( ).getBounds( );
+				bounds.setWidth( chart.getWidth( ) );
+				bounds.setHeight( chart.getHeight( ) );
+
+				ChartImageManager imageManager = new ChartImageManager( request.getSession( )
+						.getId( ),
+						cm,
+						chart.getFormat( ),
+						ChartConvertor.convertToEvaluator( chart ),
+						null,
+						null,
+						null );
+				File imageFile = imageManager.getImage( );
+				String imageId = imageFile.getName( );
+				imageId = imageId.substring( 0, imageId.lastIndexOf( '.' ) );
+
+				html = createEmitter( chart,
+						imageId,
+						imageManager.getRelativeImageFolder( )
+								+ "/" + imageFile.getName( ), imageManager.getImageMap( ) ).generateHTML( ); //$NON-NLS-1$
+			}
+			else
+			{
+				html = xml.replaceAll( "<", "&lt;" ).replace( ">", "&gt;" );
+				html = "<b>Invalid XML:</b><br/>" + html;
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace( );
+			html = e.getMessage( );
+		}
+		response.setContentType( "text/xml" ); //$NON-NLS-1$
+		response.setHeader( "Cache-Control", "no-cache" ); //$NON-NLS-1$ //$NON-NLS-2$
+		response.getWriter( ).write( "<chart><![CDATA[" + html + "]]></chart>" ); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	private ImageHTMLEmitter createEmitter( ChartModel chart, String id,
+			String src, String imageMap )
+	{
+		ImageHTMLEmitter emitter = new ImageHTMLEmitter( );
+		emitter.ext = chart.getFormat( );
+		emitter.height = (int) chart.getHeight( );
+		emitter.width = (int) chart.getWidth( );
+		emitter.id = id;
+		emitter.src = src;
+		emitter.imageMap = imageMap;
+		return emitter;
+	}
+
+	private String loadXML( String strUrl )
+	{
+		StringBuffer xml = new StringBuffer( );
+		try
+		{
+			InputStream is;
+			if ( strUrl.startsWith( "http:" ) ) //$NON-NLS-1$
+			{
+				URL url = new URL( strUrl );
+				is = url.openStream( );
+			}
+			else
+			{
+				File file = new File( strUrl );
+				if ( file.exists( ) )
+				{
+					is = new FileInputStream( file );
+				}
+				else
+				{
+					is = context.getResourceAsStream( strUrl );
+				}
+			}
+			if ( is != null )
+			{
+				byte[] buffer = new byte[1024];
+				int readSize = 0;
+				while ( ( readSize = is.read( buffer ) ) != -1 )
+				{
+					xml.append( new String( buffer, 0, readSize ) );
+				}
+				is.close( );
+			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace( );
+		}
+		return xml.toString( );
+	}
+}
