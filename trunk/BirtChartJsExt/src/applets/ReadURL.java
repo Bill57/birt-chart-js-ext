171a39f75b26f89050078b5c7e2b74e96a9a4d7c
lionel.wyl <lionel.wyl@c5db784a-11d7-11de-8090-533a57e34bb9>
2009-03-24 06:55:06 +0000

    Update ReadURL applet source according to class decompile

--- trunk/BirtChartJsExt/src/applets/ReadURL.java
+++ trunk/BirtChartJsExt/src/applets/ReadURL.java
@@ -1,45 +1,128 @@
+
 package applets;
 
 import java.applet.Applet;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.Graphics;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.URL;
 
-public class ReadURL extends Applet {
+/**
+ * 
+ * This applet is used to read content from file specified by URL
+ */
+public class ReadURL extends Applet implements Runnable
+{
+
 	/**
-	 * 
+	 * Comment for <code>serialVersionUID</code>
 	 */
-	private static final long serialVersionUID = 6887700004172624409L;
+	private static final long serialVersionUID = 2511821048048551820L;
+	public String fileURL;
+	public String message;
+	public String fileContent;
+	public int finished;
+	public int nOfReads;
+	public int nOfLines;
+	InputStream is;
+	DataInputStream dis;
+	Thread myThread;
 
-	public int finished = 0;
-
-	public String fileContent = "";
-
-	public void readFile(String filePath) {
+	public void init( )
+	{
+		fileURL = null;
+		message = "";
+		fileContent = "";
 		finished = 0;
-		System.out.println(filePath);
-		File file = new File(filePath);
-		if (file.exists()) {
-			try {
-				FileReader reader = new FileReader(file);
-				StringBuffer str = new StringBuffer();
-				char[] buffer = new char[1024];
-				int readSize = 0;
-				while ((readSize = reader.read(buffer)) != -1) {
-					str.append(buffer, 0, readSize);
+		nOfReads = 0;
+		nOfLines = 0;
+		fileURL = getParameter( "fileURL" );
+		if ( fileURL == null )
+		{
+			finished = 1;
+		}
+		myThread = new Thread( this );
+	}
+
+	public void run( )
+	{
+		do
+		{
+			if ( finished == 0 )
+			{
+				try
+				{
+					Thread.currentThread( ).setPriority( 1 );
+					is = new URL( getDocumentBase( ), fileURL ).openStream( );
+					dis = new DataInputStream( is );
+					String s = "";
+					fileContent = "";
+					nOfLines = 0;
+					do
+					{
+						String s1 = dis.readLine( );
+						if ( s1 == null )
+						{
+							is.close( );
+							break;
+						}
+						fileContent += s1 + "\r\n";
+						nOfLines++;
+					} while ( true );
 				}
-				reader.close();
-				fileContent = str.toString();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
+				catch ( Exception exception )
+				{
+					message = exception.toString( );
+					repaint( );
+				}
+				nOfReads++;
 				finished = 1;
 			}
+			try
+			{
+				Thread.sleep( 10L );
+			}
+			catch ( InterruptedException interruptedexception )
+			{
+				message = interruptedexception.toString( );
+				repaint( );
+			}
+		} while ( true );
+	}
+
+	public void start( )
+	{
+		if ( myThread != null )
+		{
+			myThread.start( );
 		}
 	}
+
+	public int readFile( String s )
+	{
+		if ( finished == 0 )
+		{
+			return 0;
+		}
+		else
+		{
+			fileURL = s;
+			finished = 0;
+			return 1;
+		}
+	}
+
+	public void stop( )
+	{
+		if ( myThread != null )
+		{
+			myThread.stop( );
+		}
+	}
+
+	public void paint( Graphics g )
+	{
+		g.drawString( message, 20, 30 );
+	}
+
 }
